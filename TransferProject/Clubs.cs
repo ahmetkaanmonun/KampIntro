6a0e4223bffbf6b5f7d1e8757a4470881d585f9b
ahmetkaanmonun <ahmetkaanmonun@users.noreply.github.com>
2021-01-17 13:53:06 +0300

    16 Ocak Ders Sonu

---
+++ TransferProject/Clubs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransferProject
+{
+    class Clubs
+    {
+
+        public string ClubName { get; set; }
+
+        public string ClubCoach { get; set; }
+
+        public string ClubCountry { get; set; }
+    }
+}
