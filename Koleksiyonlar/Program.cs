1916ba4fd179d8d11b2df6046de8ef5dfff916e2
ahmetkaanmonun <ahmetkaanmonun@users.noreply.github.com>
2021-01-20 23:50:30 +0300

    20.01.2020

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///*
+            //string[] isimler = new string[] {"Engin","Murat","Kerem","Halil"};
+            //Console.WriteLine(isimler[0]);
+            //isimler[4] = "İlker";
+            //*/
+
+            //Collections 
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+
+
+
+
+        }
+    }
+}
